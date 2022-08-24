4360bdc3e6dfa8215fa27fda14f2dad7f22b1348
Antonio Oliveira - DEV <63135415+3636-sergioharpazo@users.noreply.github.com>
2022-08-24 12:56:06 -0300

    Add files via upload

---
+++ include/sec_image.php
@@ -0,0 +1,17 @@
+<?php
+ 
+?><?php
+   require('security_image.php');
+   
+   session_start();
+    
+   $oSecurityImage = new SecurityImage(150, 30);
+   if ($oSecurityImage->Create()) 
+   {
+          $_SESSION['code'] = md5($oSecurityImage->GetCode());
+   }
+    else 
+	{
+      echo 'Image GIF library is not installed.';
+   }
+?>
